43417904db2d5d8595993afdc7337dda8649433e
mgaangel10 <mg.aangel10@gmail.com>
2023-10-04 09:55:34 +0200

    subido

---
+++ ejercicioParejaDto/ejercicioParejaDto/src/main/java/com/example/ejercicioParejaDto/EjercicioParejaDtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioParejaDto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioParejaDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioParejaDtoApplication.class, args);
+	}
+
+}
